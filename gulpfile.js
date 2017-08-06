d6df7d3df3c674fd4f10173e3096c7c7c161c666
williamded <williamded@msn.com>
2017-08-06 01:47:59 -0300

    finalizado sobre

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
+
+
+//gulp.task('sass', function () {
+//    return gulp.src('sass/**/*.sass')
+//        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//        .pipe(gulp.dest('css'));
+//})
+
 gulp.task('sass', function () {
     return gulp.src('sass/**/*.sass')
         .pipe(sass().on('error', sass.logError))
